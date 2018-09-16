c0d6b696b429bb650de05187dd2dd9c833417472
guildsmac <guildsmac@gmail.com>
2018-09-16 00:41:09 -0300

    Finished About page

--- scripts/aboutScripts.js
+++ scripts/aboutScripts.js
@@ -1,8 +1,29 @@
-function expandAboutUs(){
-        let component = $('.card');
-        if(component.is(':visible'))
-            component.fadeOut(200);
+const ABOUT_US_CARD = $('#about-us .card');
+const ABOUT_PROJECT_CARD = $('#about-project .card');
 
-        else
-            component.fadeIn(200);
+function toggleAboutUs(caller) {
+    if (ABOUT_US_CARD.is(':visible'))
+        ABOUT_US_CARD.fadeOut(200);
+
+    else
+        ABOUT_US_CARD.fadeIn(200);
+
+    if (caller === 'html') {
+        if (ABOUT_PROJECT_CARD.is(':visible'))
+            toggleAboutProject('script');
+    }
+    window.scrollTo(0, 0);
+}
+
+function toggleAboutProject(caller) {
+    if (ABOUT_PROJECT_CARD.is(':visible'))
+        ABOUT_PROJECT_CARD.fadeOut(200);
+    else
+        ABOUT_PROJECT_CARD.fadeIn(200);
+
+    if (caller === 'html') {
+        if (ABOUT_US_CARD.is(':visible'))
+            toggleAboutUs('script');
+    }
+    window.scrollTo(0, 0);
 }
